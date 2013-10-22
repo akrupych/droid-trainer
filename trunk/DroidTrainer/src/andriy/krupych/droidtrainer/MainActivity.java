70e1122632f2c9ff0d4e0bd7e361857a0ab26df8
andriy.krupych@gmail.com <andriy.krupych@gmail.com@e82d2ce2-000e-83e0-a138-29a09c393c8d>
2013-10-22 23:19:10 +0000

    removed action bar and title bar

--- trunk/DroidTrainer/src/andriy/krupych/droidtrainer/MainActivity.java
+++ trunk/DroidTrainer/src/andriy/krupych/droidtrainer/MainActivity.java
@@ -2,18 +2,14 @@ package andriy.krupych.droidtrainer;
 
 import java.util.Locale;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
